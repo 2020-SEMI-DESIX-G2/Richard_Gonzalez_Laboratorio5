1cdd63e762571e24c03b074ed86d5492a58c8a14
Richard González <richard.gonzalez2@utp.ac.pa>
2020-08-03 21:24:25 -0500

    Codigo JS

---
+++ Lab5.js
@@ -0,0 +1,27 @@
+var a = 35;
+var b = 5;
+var c = a+b;
+console.log('La suma de 35 y 5 es:' + (a+b));
+console.log('La suma de 35 menos 5 es:' + (a-b));
+console.log('La multiplicación de 35 y 5 es:' + (a*b));
+console.log('La división de 35 y 5 es:' + (a/b));
+
+let x = 'hola';
+let y = 'MUNDO';
+console.log ('Resultado del let: ' + x +' '+ y);
+
+const R = 7;
+const P = 'siete';
+console.log('El resultado de tipo de datos de 7 y siete es: ' + typeof R +' '+ typeof P);
+
+var t=77;
+var objeto = {
+	entero: t, 
+	string: 'Soy String', 
+	boolean: true, 
+	vacio: ''};
+console.log('El resultado de varibale tipo objeto es: ');
+console.log(objeto.entero);
+console.log(objeto.string);
+console.log(objeto.boolean);
+console.log(objeto.vacio);
